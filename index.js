8d9690d3e95f313ba6b6ed13a7198767884b871b
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-16 09:47:03 -0300

    se creo la dependencia package.json

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ console.log(`Prueba3`);
 
 const base = 2;
 
+console.log(`================= 
+Tabla del ${base}
+====================`);
+
 for (let i = 1; i <= 10; i++) {
     console.log(`${base} * ${i} = ${base * i}`);
 }
