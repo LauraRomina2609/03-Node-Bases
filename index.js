dbba13285e36638224f4a6d8f193cfb1291d37ed
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-19 00:42:53 -0300

    tabla del 3

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 console.log(`Prueba3`);
 
 
-const base = 2;
+const base = 3;
 
 console.log(`================= 
 Tabla del ${base}
 ====================`);
 
-let resultado;
+let resultado = ` `;
 
 for (let i = 1; i <= 10; i++) {
-    resultado += `${base} * ${i} = ${base * i}`
+    resultado += `${base} * ${i} = ${base * i}\n`
 }
 console.log(resultado);
 fs.writeFileSync(`tablas/tabla-del-${base}`, resultado);
