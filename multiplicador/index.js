1bbc8020b32c252a35858c8e46a23460c4f7b74e
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-19 11:01:03 -0300

    multiplicador

---
+++ multiplicador/index.js
@@ -0,0 +1,16 @@
+
+
+const multiplicar = (base) => {
+    console.log(`==================`);
+    console.log(`Tabla del ${base}`); 
+    console.log(`==================`);
+
+    let resultado = ` `;
+
+    for (let i = 1; i <= 10; i++) {
+        resultado += `${base} * ${i} = ${base * i}\n`
+    }
+    
+    console.log(resultado);
+    fs.writeFileSync(`tablas/tabla-del-${base}`, resultado);
+}
