820e72382cb207d9b360fbc303f67b9b2555e7cc
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-19 01:09:45 -0300

    tabla del 3

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const fs = require('fs');
+const multiplicador = require(`./`)
 
 console.log(`Prueba3`);
 
 
 const base = 3;
 
-console.log(`================= 
-Tabla del ${base}
-====================`);
-
-let resultado = ` `;
-
-for (let i = 1; i <= 10; i++) {
-    resultado += `${base} * ${i} = ${base * i}\n`
-}
-console.log(resultado);
-fs.writeFileSync(`tablas/tabla-del-${base}`, resultado);
+multiplicar(base);
