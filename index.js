101d53031e77f7837e533d6215916e73d2552171
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-22 12:53:25 -0300

    Avance del multiplicador

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const argv = require(`yargs/yargs`)(process.argv.slice(2)).argv;
 const {multiplicar} = require(`./multiplicador`);
-
-console.log(argv._;
 
 const base = 5;
 
-/*multiplicar(base);
+multiplicar(base);
