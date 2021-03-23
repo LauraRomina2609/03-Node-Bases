334d9d17cefe633cb467b4aff108846fe06d3887
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-23 10:36:05 -0300

    Avances del mutiplicador

--- multiplicador/index.js
+++ multiplicador/index.js
@@ -9,7 +9,7 @@ const multiplicar = (base) => {
     const resultado = logica(base);
         
     console.log(resultado);
-    const nombreArchivo = `tabla-del-${base}`
+    const nombreArchivo = `tabla-del-${base}.txt`
     fs.writeFile(`tablas/${nombreArchivo}`, resultado, (err) => {
         if (err) throw err;
         console.log(`El archivo ${nombreArchivo} fue creado`);
