dd260c3883b13a880725e0a3d492d1166b372fe6
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-24 21:07:42 -0300

    argv

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const argv = require(`yargs`).argv;
-const {multiplicar} = require("./multiplicador");
+const argv = require(`yargs`)
+
+            .option(`b`, {
+            alias: `base`,
+            type: `number`,
+            demandOption: true,
+            describe: `es la base de la tabla`
+            })
+
+            .argv;
+const {multiplicar} = require(`./multiplicador`);
 
 console.log(argv);
 
